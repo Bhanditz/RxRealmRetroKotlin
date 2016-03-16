34abdfddd20456bbb222d425794d76406de53afa
Ahmed Rizwan <ahmedrizwan@outlook.com>
2016-03-16 11:44:47 +0500

    Add public_repos to the Activity

--- app/src/main/java/com/minimize/android/rxretrorealm/MainActivity.kt
+++ app/src/main/java/com/minimize/android/rxretrorealm/MainActivity.kt
@@ -1,20 +1,29 @@
 package com.minimize.android.rxretrorealm
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.bumptech.glide.Glide
 import com.google.gson.ExclusionStrategy
 import com.google.gson.FieldAttributes
 import com.google.gson.GsonBuilder
+import com.minimize.android.rxretrorealm.databinding.ActivityMainBinding
+import io.realm.Realm
 import io.realm.RealmObject
+import io.realm.RealmQuery
 import retrofit.GsonConverterFactory
 import retrofit.Retrofit
 import retrofit.RxJavaCallAdapterFactory
+import rx.android.schedulers.AndroidSchedulers
+import rx.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this,
+        R.layout.activity_main)
 
     val gson = GsonBuilder().setExclusionStrategies(object : ExclusionStrategy {
       override fun shouldSkipField(f: FieldAttributes): Boolean {
@@ -35,7 +44,36 @@ class MainActivity : AppCompatActivity() {
     val githubService: GithubService = retrofit.create(
         GithubService::class.java)
 
+    val realm = Realm.getDefaultInstance()
 
+    //get user if it's already saved
+    val savedUser: Github? = RealmQuery.createQuery(realm,
+        Github::class.java).findFirst()
+    updateViews(binding, savedUser)
+
+    githubService.getGithubUser("ahmedrizwan")
+        .subscribeOn(Schedulers.newThread())
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe(
+            { user ->
+              realm.beginTransaction()
+              realm.copyToRealmOrUpdate(user)
+              realm.commitTransaction()
+              updateViews(binding, user)
+            },
+            { error ->
+              Log.e("Error", error.message)
+            }
+        )
+
+
+  }
+
+  private fun updateViews(binding: ActivityMainBinding, savedUser: Github?) {
+    Glide.with(this).load(savedUser?.avatarUrl).into(binding.userImage)
+    binding.userName.text = savedUser?.name
+    binding.publicRepos.text = "Public Repos: "+ savedUser?.publicRepos
+        .toString()
   }
 }
 
